c319a4e9fb03907c54ebc2ce290f0b68ead49962
TangQi <tangqi@quancheng-ec.com>
2017-07-05 14:25:55 +0800

    update

--- todos/app/src/main/java/com/jimmy/todos/SplashActivity.java
+++ todos/app/src/main/java/com/jimmy/todos/SplashActivity.java
@@ -3,12 +3,12 @@ package com.jimmy.todos;
 import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
-import android.app.Activity;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import com.jimmy.todos.base.BaseActivity;
 import com.jimmy.todos.databinding.SplashBinding;
 
 import java.util.Timer;
@@ -18,7 +18,7 @@ import java.util.TimerTask;
  * Created by Jimmy on 2017/7/4.
  */
 
-public class SplashActivity extends Activity {
+public class SplashActivity extends BaseActivity {
 
     private SplashBinding binding;
 
@@ -47,16 +47,7 @@ public class SplashActivity extends Activity {
 
             @Override
             public void onAnimationEnd(Animator animation) {
-                TimerTask task = new TimerTask() {
-                    @Override
-                    public void run() {
-                        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                        startActivity(intent);
-                        finish();
-                    }
-                };
-                Timer timer = new Timer();
-                timer.schedule(task, 1000);
+                naviToHome();
             }
 
             @Override
@@ -69,5 +60,18 @@ public class SplashActivity extends Activity {
 
             }
         });
+    }
+
+    private void naviToHome() {
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(task, 1000);
     }
 }
